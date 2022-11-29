70614983402d6ec86c79338b971edaadaa801340
Lucas Christenson <megal2cas@gmail.com>
2022-11-28 16:36:35 -0800

    implement box-plane function

---
+++ src/intersection.cpp
@@ -0,0 +1,14 @@
+#include <intersection.h>
+#include <glm/normalize.hpp>
+
+bool Plane_Plane(Face P1, Face P2)
+{
+
+	return false;
+}
+
+bool Box_Plane(Hitbox box, Face plane)
+{
+
+	return false;
+}
